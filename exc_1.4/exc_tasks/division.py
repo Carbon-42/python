b91efeea55b18c7de76a220c70fbe5ec7cc9edb2
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-09-11 19:25:35 -0600

    Create division.py

---
+++ exc_1.4/exc_tasks/division.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number to be divided: "))
+b = int(input("Enter another number to divide it by: "))
+try:
+    print(a/b)
+except:
+    print("There's some error in your input values")
+print("Program complete!")
